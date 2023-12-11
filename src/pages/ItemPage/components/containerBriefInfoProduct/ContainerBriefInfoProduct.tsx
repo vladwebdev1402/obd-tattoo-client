81e0289e0ba82af034ce2f2f49d6fea9256f0e17
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2023-12-11 10:30:26 +0700

    модальное окно на странице айтемов

--- src/pages/ItemPage/components/containerBriefInfoProduct/ContainerBriefInfoProduct.tsx
+++ src/pages/ItemPage/components/containerBriefInfoProduct/ContainerBriefInfoProduct.tsx
@@ -3,13 +3,17 @@ import st from "./container.module.scss";
 import { IShopItem } from "@/types/shopItem";
 import BtnChangeCount from "@/components/btnChangeCount/BtnChangeCount";
 import ClipButton from "@UI/button/clipButton/ClipButton";
+import ModalAuth from "@/components/modalAuth/ModalAuth";
+import AuthStore from "@/store/AuthStore/AuthStore";
 interface Props {
   item: IShopItem;
 }
 
 const ContainerBriefInfoProduct: FC<Props> = ({ item }) => {
+  const [modal, setModal] = useState(false);
   return (
     <div className={st.containerBriefInfo}>
+      {modal && <ModalAuth setModal={setModal} />}
       <div className={st.itemNameTxt}>{item.name}</div>
       <div className={st.priceBlock}>
         <span className={st.currentPrice}>
@@ -36,7 +40,12 @@ const ContainerBriefInfoProduct: FC<Props> = ({ item }) => {
         <div className={st.utilsBasket}>
           <BtnChangeCount item={item} className={st.desktopChangeCount} />
           <div className={st.addInBasketBtn}>
-            <ClipButton theme="dark" onClick={() => {}}>
+            <ClipButton
+              theme="dark"
+              onClick={() => {
+                if (!AuthStore.auth) setModal(!modal);
+              }}
+            >
               Добавить в корзину
             </ClipButton>
           </div>
