9ffcd186a20e8e7c3a15c085dd34f7b4814a298f
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2023-12-11 11:06:28 +0700

    страница брендов

--- src/pages/BrandPage/BrandPage.tsx
+++ src/pages/BrandPage/BrandPage.tsx
@@ -4,37 +4,34 @@ import Breadcrumbs from "@/components/breadcrumbs/Breadcrumbs";
 import FilterCategoryCatalog from "@/components/filterCategoryCatalog/FilterCategoryCatalog";
 import ShopItem from "@/components/ShopItem/ShopItem";
 import ClipButton from "@/components/UI/button/clipButton/ClipButton";
-import { brands } from "@/data/catalogCategory";
-import { ICategory } from "@/types/category";
-import { IShopItem } from "@/types/shopItem";
 import st from "./BrandPage.module.scss";
 import ItemsContainer from "@/components/UI/containers/ItemsContainer/ItemsContainer";
 import BrandImgHeader from "./components/brandImgHeader/BrandImgHeader";
-import { ItemStore } from "@/store";
-const BrandPage = () => {
+import { BrandStore, ItemStore } from "@/store";
+import { observer } from "mobx-react-lite";
+import { IItemParams } from "@/types/itemParamsApi";
+const BrandPage = observer(() => {
   const params = useParams<{ id: string }>();
-  const [brand, setBrand] = useState<ICategory>();
+  const brand = BrandStore.data.filter((b) => b._id === params.id)[0];
   const [currentType, setCurrentType] = useState("all");
-  const [items, setItems] = useState<IShopItem[]>([]);
-  const [filterItems, setFilterItems] = useState<IShopItem[]>([]);
 
   useEffect(() => {
     window.scrollTo({ top: 120, behavior: "smooth" });
   }, []);
 
   useEffect(() => {
-    const brand = brands.filter((brand) => brand.id === Number(params.id))[0];
-    setBrand(brand);
-  }, [params]);
+    const query: IItemParams = {};
+    if (currentType !== "all") query.category = currentType;
+    if (BrandStore.isLoadingComplete)
+      ItemStore.getItems({ ...query, brand: brand._id });
+  }, [BrandStore.isLoadingComplete, params, currentType]);
 
-  useEffect(() => {
-    if (currentType == "all") setFilterItems(items);
-    else setFilterItems(items.filter((item) => item.category === currentType));
-  }, [currentType, items]);
   return (
     <div className={st.container}>
-      <Breadcrumbs />
-      <BrandImgHeader img={brand?.img || ""} name={brand?.name || ""} />
+      <Breadcrumbs params={[brand?.name]} />
+      {BrandStore.isLoadingComplete && (
+        <BrandImgHeader img={brand.image} name={brand?.name || ""} />
+      )}
       <FilterCategoryCatalog
         setType={setCurrentType}
         currentType={currentType}
@@ -44,20 +41,20 @@ const BrandPage = () => {
         error={ItemStore.error}
         isLoadingComplete={ItemStore.isLoadingComplete}
       >
-        {filterItems.length ? (
-          filterItems.map((item) => <ShopItem item={item} key={item._id} />)
+        {ItemStore.data.length ? (
+          ItemStore.data.map((item) => <ShopItem item={item} key={item._id} />)
         ) : (
           <h1>Товары этой категории от данного бренда отсутствуют</h1>
         )}
       </ItemsContainer>
 
-      {filterItems.length > 0 && (
+      {ItemStore.data.length > 0 && ItemStore.data.length % 8 == 0 && (
         <ClipButton theme="light" onClick={() => {}} className={st.btn}>
           Показать ещё
         </ClipButton>
       )}
     </div>
   );
-};
+});
 
 export default BrandPage;
--- src/types/IBrand.ts
+++ src/types/IBrand.ts
@@ -1,5 +1,5 @@
 export interface IBrand {
     _id: string;
     name: string;
-    img: string;
+    image: string;
 }
